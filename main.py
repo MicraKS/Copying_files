25bf30685b71675b274576ad6731ffad6c3c1d18
MicraKS <5328207@gmail.com>
2023-04-27 19:14:25 +0300

    in work

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import os
 import shutil
-
+import pathlib
 
 path = 'D:/thisTest/'
 path2 = 'D:/test2/'
-os.chdir(path)
-for i in os.listdir(path):
-    os.chdir(path)
-    if os.path.isfile(i):
-        shutil.copy2(i, path2)
-    elif os.path.isdir(i):
-        folder = os.path.basename(r'D:/thisTest/' + i)
-        os.chdir(path2)
-        os.mkdir(folder)
-        for i in os.listdir(path + folder):
-            os.chdir(path + folder)
-            z = path2 + folder
-            shutil.copy2(i, z)
 
+def copy_files(path,path2):
+    os.chdir(path)
+    for i in os.listdir(path):
+        if os.path.isfile(i):
+            shutil.copy2(i, path2)
+def copy_folder(path, path2, files):
+    for i in files:
+        os.chdir(path2)
+        os.mkdir(i)
+def search_dir(path):
+    example_dir = 'D:/thisTest/'
+    dir = pathlib.Path(example_dir)
+    files = [file.name for file in dir.iterdir() if file.is_dir()]
+    return files
+
+copy_folder(path, path2, search_dir(path))
